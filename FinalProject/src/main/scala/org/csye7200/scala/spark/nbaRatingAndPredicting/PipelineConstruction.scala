fbfe394387a3f6c466cf3f1ae81ca641259a6160
ll1195831146 <liu.le@husky.neu.edu>
2018-12-03 19:23:39 -0500

    StringIndexer, VectorAssembler

--- FinalProject/src/main/scala/org/csye7200/scala/spark/nbaRatingAndPredicting/PipelineConstruction.scala
+++ FinalProject/src/main/scala/org/csye7200/scala/spark/nbaRatingAndPredicting/PipelineConstruction.scala
@@ -4,18 +4,63 @@ import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
 
 object PipelineConstruction {
 
-//  val MVP_index: StringIndexer = new StringIndexer()
-//    .setInputCol("MVP")
-//    .setOutputCol("pre_MVP")
-//
-//
-//  // Create the feature columns
-//  val featureColumns: Array[String] = Array("bmi", "MVP", "pre_MVP")
-//
-//  // Use the VectorAssembly to create the feature vector
-//  val vectorFeatureAssembler = new VectorAssembler()
-//    .setInputCols(featureColumns)
-//    .setOutputCol("features")
+  val MVP_index: StringIndexer = new StringIndexer()
+    .setInputCol("Voting")
+    .setOutputCol("IndexedVoting")
+
+
+  // Create the feature columns
+  val featureColumns: Array[String] = Array(
+    "Year",
+    "Age",
+    "G",
+    "GS",
+    "MP",
+    "PER",
+    "TS%",
+    "3PAr",
+    "FTr",
+    "ORB",
+    "DRB%",
+    "TRB%",
+    "AST%",
+    "STL%",
+    "BLK%",
+    "TOV%",
+    "USG%",
+    "OWS",
+    "DWS",
+    "WS",
+    "WS/48",
+    "OBPM",
+    "DBPM",
+    "BPM",
+    "VORP",
+    "FG",
+    "FGA",
+    "FG",
+    "3P",
+    "3PA",
+    "3P%",
+    "2P",
+    "2P%",
+    "eFG%",
+    "FT",
+    "FTA",
+    "FT%",
+    "ORB",
+    "DRB",
+    "TRB",
+    "AST",
+    "STL",
+    "BLK",
+    "TOV"
+  )
+
+  // Use the VectorAssembly to create the feature vector
+  val vectorFeatureAssembler = new VectorAssembler()
+    .setInputCols(featureColumns)
+    .setOutputCol("features")
 
 
 }
